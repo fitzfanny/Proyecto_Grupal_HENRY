078ade9cd57918cb1acdd43e9cd338d6b2b3a8c4
Tinmarian <tinmar96@gmail.com>
2023-09-21 20:14:12 -0600

    debugging dag_gd_to_gcs

--- dags/dag_gd_to_gcs.py
+++ dags/dag_gd_to_gcs.py
@@ -12,31 +12,31 @@ HENRY_PROJECT = 'fiery-protocol-399500'
 
 
 default_args = {
-        'owner':'Tinmar Andrade',
-        'start_date':datetime(2023,9,20),
-        'email':['tinmar96@gmail.com','jozz.rom@gmail.com'],
-        'email_on_failure':True,
-    }
+		'owner':'Tinmar Andrade',
+		'start_date':datetime(2023,9,20),
+		'email':['tinmar96@gmail.com','jozz.rom@gmail.com'],
+		'email_on_failure':True,
+	}
 
 
 @dag(
-    'gd_to_gcs',
-    default_args=default_args,
-    catchup=False,
-    schedule_interval=None,
-    tags=['HENRY','Proyecto Final','Proyecto en Equipo']
-    )
+	'gd_to_gcs',
+	default_args=default_args,
+	catchup=False,
+	schedule_interval=None,
+	tags=['HENRY','Proyecto Final','Proyecto en Equipo']
+	)
 
 
 def gd_to_gcs():
-    
-    create_bucket = GCSCreateBucketOperator(
-        task_id = 'create_bucket',
+
+	create_bucket = GCSCreateBucketOperator(
+		task_id = 'create_bucket',
 		bucket_name = MY_BUCKET_NAME,
 		location = 'us-east1',
 		project_id = HENRY_PROJECT,
 		gcp_conn_id= GCPCONN
-		)
+	)
 
 	# Carga de datos Yelp
 	MY_FOLDER_ID = '1TI-SsMnZsNP6t930olEEWbBQdo_yuIZF' # Folder de Yelp
@@ -49,7 +49,7 @@ def gd_to_gcs():
 			folder_id=MY_FOLDER_ID,
 			drive_id="None",
 			gcp_conn_id=GCPCONN
-			)
+		)
 
 	# Cargar datos de Maps Metadata
 	MY_FOLDER_ID = '1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z'
@@ -62,7 +62,7 @@ def gd_to_gcs():
 			folder_id=MY_FOLDER_ID,
 			drive_id="None",
 			gcp_conn_id=GCPCONN
-			)
+		)
 
 	# Cargar datos de Maps Estados
 	for OBJECT_NAME,MY_FOLDER_ID in zip(['review-New_York','review-California','review-Texas','review-Colorado','review-Georgia'],['18HYLDXcKg-cC1CT9vkRUCgea04cNpV33','1Jrbjt-0hnLCvecfrnMwGu1jYZSxElJll','1zq12pojMW2zeGgts0lHFSf_pF1L_4UWr','1IlUZJZxOyRiIWo3G6BqW1Thu2kXYKMFX','1MuPznes6CebS6gyWPVU-kR4EVKKLY4l3']):
@@ -76,7 +76,7 @@ def gd_to_gcs():
 					folder_id=MY_FOLDER_ID,
 					drive_id="None",
 					gcp_conn_id=GCPCONN
-					)
+				)
 
 		if OBJECT_NAME == 'review-California':
 			for MY_FILE_NAME in range(1,19):
@@ -88,7 +88,7 @@ def gd_to_gcs():
 					folder_id=MY_FOLDER_ID,
 					drive_id="None",
 					gcp_conn_id=GCPCONN
-					)
+				)
 
 		if OBJECT_NAME == 'review-Texas':
 			for MY_FILE_NAME in range(1,17):
@@ -112,20 +112,20 @@ def gd_to_gcs():
 					folder_id=MY_FOLDER_ID,
 					drive_id="None",
 					gcp_conn_id=GCPCONN
-					)
+				)
 
 		if OBJECT_NAME == 'review-Georgia':
 			for MY_FILE_NAME in range(1,14):
 				extract_load_maps_georgia = GoogleDriveToGCSOperator(
-        			task_id = 'extract_load_maps_georgia',
+					task_id = 'extract_load_maps_georgia',
 					bucket_name=MY_BUCKET_NAME,
 					object_name=f'{OBJECT_NAME[7:]}_{MY_FILE_NAME}.json',
 					file_name=f'{MY_FILE_NAME}.json',
 					folder_id=MY_FOLDER_ID,
 					drive_id="None",
 					gcp_conn_id=GCPCONN
-					)
-    
-    
+				)
+
+	create_bucket >> extract_load_yelp >> extract_load_maps_meta >> extract_load_maps_newyork >> extract_load_maps_california >> extract_load_maps_texas >> extract_load_maps_colorado >> extract_load_maps_georgia
 
 dag = gd_to_gcs()
